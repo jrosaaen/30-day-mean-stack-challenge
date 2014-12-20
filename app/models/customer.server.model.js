cea418f3269a590be30ca05027284afc09a960b8
Joshua Rosaaen <joshua.rosaaen@gmail.com>
2014-12-19 17:59:57 -0800

    added some detail to customer model

--- app/models/customer.server.model.js
+++ app/models/customer.server.model.js
@@ -10,10 +10,49 @@ var mongoose = require('mongoose'),
  * Customer Schema
  */
 var CustomerSchema = new Schema({
-	name: {
+	firstName: {
 		type: String,
 		default: '',
-		required: 'Please fill Customer name',
+		trim: true
+	},
+	lastName: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	city: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	country: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	industry: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	email: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	phone: {
+		type: String,
+		default: '',
+		trim: true
+	},
+  referred: {
+		type: Boolean,
+		default: '',
+		trim: true
+	},
+	channel: {
+		type: String,
+		default: '',
 		trim: true
 	},
 	created: {
